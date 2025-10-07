6cd99ac1368ef1c0c511eaf18bab102a9d3863e8
Nyscat <nyscat@gmail.com>
2025-10-07 11:29:41 +1000

    Add files via upload

---
+++ skyfarm-config.js
@@ -0,0 +1,9 @@
+window.SKYFARM_CONFIG = {
+  "apiKey": "AIzaSyBO9_0v1HdvVDuK3EFvaBGvQJIGvBwmW70",
+  "authDomain": "consentes-pastoral.firebaseapp.com",
+  "projectId": "consentes-pastoral",
+  "storageBucket": "consentes-pastoral.firebasestorage.app",
+  "messagingSenderId": "494858780013",
+  "appId": "1:494858780013:web:d87497b370c82eeab06e64",
+  "measurementId": "G-9MSLTVW70N"
+};
